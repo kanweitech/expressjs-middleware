59e6e776c1f08bec05da9366cfc134ac1ef8cb2c
kanweitech <kanweiedward@gmail.com>
2024-09-12 08:12:25 -0700

    router-level middleware

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 5000;
 
+const router = express.Router()
+
 // Application-level middleware
 const loggerMiddleware = (req, res, next) => {
     console.log(`${new Date()} === Request [${req.method}] [${req.url}]`);
@@ -13,6 +15,17 @@ const loggerMiddleware = (req, res, next) => {
 app.use(loggerMiddleware);
 // Third party middleware
 // Router-level middleware
+
+app.use("/api/users/", router);
+const getUsers = (req, res) => {
+    res.json({ message: "Get all user"});
+};
+
+const createUser = (req, res) => {
+    res.json({ message: "Create new user"});
+};
+
+router.route("/").get(getUsers).post(createUser);
 // Built-in middleware
 // error-handling middleware
 

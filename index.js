68c8e8af9bf6fe31118ce3bd043d2899d5f5d6d7
kanweitech <kanweiedward@gmail.com>
2024-09-14 07:31:07 -0700

    error-handling middleware

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const loggerMiddleware = (req, res, next) => {
 app.use(loggerMiddleware);
 // Third party middleware
 // Router-level middleware
-
 app.use("/api/users", router);
 
 const fakeAuth = (req, res, next) => {
@@ -25,7 +24,7 @@ const fakeAuth = (req, res, next) => {
         next();
     } else {
         res.status(401);
-        throw new Error("Users is not authorized!");
+        throw new Error("User is not authorized!");
     }
 };
 
@@ -34,12 +33,42 @@ const getUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
+    console.log("This is the request body received from client : ", req.body);
     res.json({ message: "Create new user"});
 };
 router.use(fakeAuth);
 router.route("/").get(getUsers).post(createUser);
 // Built-in middleware
 // error-handling middleware
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.status(statusCode);
+    switch (statusCode) {
+        case 401:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+            });
+            break;
+        case 404:
+            res.json({
+                title: "Not Found",
+                message: err.message,
+            });
+            break;
+        case 500:
+            res.json({
+                title: "Server Error",
+                message: err.message,
+            });
+            break; 
+
+        default:
+            break;
+    }
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
